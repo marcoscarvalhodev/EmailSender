d789b14e2ea8ee63b41d67e292417b4ca137497f
Marcos Carvalho <marcoscarvalhodev99@gmail.com>
2024-11-15 11:54:12 -0300

    Add project files.

---
+++ EmailSender/Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using EmailSender.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailSender.Controllers
+{
+    [Route("api/emails")]
+    [ApiController]
+    public class EmailController:ControllerBase
+    {
+        private readonly IEmailService emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> SendEmail(string name, string receptor, string subject, string body)
+        {
+            await emailService.SendEmail(name, receptor, subject, body);
+
+            return Ok();
+        }
+    }
+}
---
+++ EmailSender/Services/EmailService.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace EmailSender.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmail(string name, string receptor, string subject, string body);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task SendEmail(string name, string receptor, string subject, string body)
+        {
+            var email = configuration.GetValue<string>("EMAIL_CONFIGURATION:EMAIL");
+            var password = configuration.GetValue<string>("EMAIL_CONFIGURATION:PASSWORD");
+            var host = configuration.GetValue<string>("EMAIL_CONFIGURATION:HOST");
+            var port = configuration.GetValue<int>("EMAIL_CONFIGURATION:PORT");
+
+            var smtpClient = new SmtpClient(host, port);
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+
+            smtpClient.Credentials = new NetworkCredential(email, password);
+
+            var fullBody = "from: " + name + " (" + receptor + ")" + "\n\n" + body;
+
+            var message = new MailMessage(receptor, email!, subject, fullBody);
+
+            await smtpClient.SendMailAsync(message);
+
+        }
+    }
+}
